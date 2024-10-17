aaa5ecbaf3efe2d39ba85efba0f3299bdaf5d5c8
Vito <vi.h.seo@gmail.com>
2024-10-17 23:36:57 +0200

    poc - translation

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,49 +2,92 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "CertainWager",
-  tagline: "Top-quality Hardware Tools",
+  tagline: "List of the best online betting sites and casinos",
   description:
-    "CertainWager offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+    "CertainWager showcases the best online betting sites and casinos. Read reviews and compare offers. Learn how to maximize your profits with our expert insights and tips.",
   description_short:
-    "CertainWager offers top-tier hardware tools and expert construction services to meet all your project needs.",
+    "CertainWager showcases the best online betting sites and casinos. Read reviews and compare offers.",
   url: "https://certainwager.com",
   author: "Viktor Hajdu",
 };
 
 export const TEXTS = {
   en: {
-    reviewTitle: "Top Betting Sites & Casinos: Reviews",
+    blogLayoutDescription:
+      "Dive into the latest news, insider tips, and winning strategies in the thrilling world of sports betting and casinos.",
+    reviewLayoutName: "Top Casinos & Sports Betting Reviews | CertainWager",
+    reviewLayoutDescription:
+      "Discover 100% real and honest reviews from experts on the best casinos and sports betting sites.",
+    mainDescription:
+      "Unlock the best free bets and exclusive deals in the world of sports betting and casinos.",
+    reviewTitle: "Top Betting Sites & Casinos: Unbiased Reviews",
     reviewDescription:
-      "Find the best online betting sites and casinos. Read reviews and compare offers.",
+      "Find and compare the best online betting sites and casinos. Read honest reviews and seize top offers.",
     blogTitle: "Your Insider's Guide to Maximizing Profits",
     blogDescription:
-      "Learn how to maximize your profits with our expert insights and tips.",
+      "Master the art of winning with our expert insights, tips, and strategies.",
     heroSubtitle:
-      "Discover the best free and exclusive deals and offers. Maximize your winnings effortlessly with CertainWager!",
-    heroAlt: "Technolgoies showcasing money and betting",
-    contactTitle: "Contact Us",
-    emailSubscribe1: "Subscribe to our newsletter to get the ",
+      "Discover the best free bets and exclusive offers. Maximize your winnings effortlessly with CertainWager!",
+    heroAlt: "Exciting visuals showcasing money and betting opportunities",
+    contactTitle: "Get in Touch with Us",
+    emailSubscribe1: "Subscribe to our newsletter and be the first to get the ",
     emailSubscribe2: "Latest Offers!",
-    subscribe: "Subscribe",
+    subscribe: "Subscribe Now",
     contactDescription:
-      "We're here to help you with any questions or concerns. Reach out to us and we'll get back to you as soon as possible.",
+      "Have questions or need assistance? Reach out, and we'll respond as soon as possible.",
+  },
+  ie: {
+    emailSubscribe1: "Subscribe to our newsletter and be the first to get the ",
+    emailSubscribe2: "Latest Offers!",
+    subscribe: "Subscribe Now",
   },
   sk: {
-    reviewTitle: "Top Betting Sites & Casinos: Reviews",
+    blogLayoutDescription:
+      "Čítajte najnovšie správy, tipy a triky zo sveta športového stávkovania a kasín.",
+    reviewLayoutName: "Recenzie Kasín a Stávkových Stránok | CertainWager",
+    reviewLayoutDescription:
+      "Objavte 100% skutočné a čestné recenzie od odborníkov na najlepšie kasína a stávkové stránky.",
+    mainDescription:
+      "Získajte najlepšie bezplatné a exkluzívne ponuky vo svete športového stávkovania a kasín.",
+    reviewTitle: "Najlepšie Stávkové Stránky a Kasína: Recenzie",
     reviewDescription:
-      "Find the best online betting sites and casinos. Read reviews and compare offers.",
-    blogTitle: "Your Insider's Guide to Maximizing Profits",
+      "Nájdite a porovnajte najlepšie online stávkové stránky a kasína. Prečítajte si recenzie a využite top ponuky.",
+    blogTitle: "Váš Sprievodca k Maximalizácii Ziskov",
     blogDescription:
-      "Learn how to maximize your profits with our expert insights and tips.",
+      "Naučte sa umenie výhry s našimi odbornými poznatkami, tipmi a stratégiami.",
     heroSubtitle:
-      "Discover the best free and exclusive deals and offers. Maximize your winnings effortlessly with CertainWager!",
-    heroAlt: "Technolgoies showcasing money and betting",
-    contactTitle: "Kontaktujte nás",
+      "Objavte najlepšie bezplatné stávky a exkluzívne ponuky. Maximalizujte svoje výhry jednoducho s CertainWager!",
+    heroAlt: "Technológie prezentujúce peniaze a stávkovanie",
+    contactTitle: "Kontaktujte Nás",
     emailSubscribe1: "Prihláste sa k odberu noviniek a získajte ",
     emailSubscribe2: "Najnovšie Ponuky!",
     subscribe: "Odoberať",
     contactDescription:
-      "We're here to help you with any questions or concerns. Reach out to us and we'll get back to you as soon as possible.",
+      "Sme tu, aby sme vám pomohli s akýmikoľvek otázkami alebo obavami. Kontaktujte nás a ozveme sa vám čo najskôr.",
+  },
+  cz: {
+    blogLayoutDescription:
+      "Čtěte nejnovější zprávy, tipy a triky ze světa sportovního sázení a kasin.",
+    reviewLayoutName: "Recenze Kasín a Sázkových Stránek | CertainWager",
+    reviewLayoutDescription:
+      "Objevte 100% skutečné a upřímné recenze od expertů na nejlepší kasina a sázkové stránky.",
+    mainDescription:
+      "Získejte nejlepší bezplatné a exkluzivní nabídky ve světě sportovního sázení a kasin.",
+    reviewTitle: "Nejlepší Sázkové Stránky a Kasina: Recenze",
+    reviewDescription:
+      "Najděte a porovnejte nejlepší online sázkové stránky a kasina. Přečtěte si recenze a využijte top nabídky.",
+    blogTitle: "Váš Průvodce k Maximalizaci Zisků",
+    blogDescription:
+      "Ovládněte umění výhry s našimi odbornými poznatky, tipy a strategiemi.",
+    heroSubtitle:
+      "Objevte nejlepší bezplatné sázky a exkluzivní nabídky. Maximalizujte své výhry snadno s CertainWager!",
+    heroAlt: "Technologie prezentující peníze a sázení",
+    contactTitle: "Kontaktujte Nás",
+    emailSubscribe1: "Přihlaste se k odběru novinek a získejte ",
+    emailSubscribe2: "Nejnovější Nabídky!",
+    subscribe: "Odebírat",
+    contactDescription:
+      "Jsme tu, abychom vám pomohli s jakýmikoli dotazy nebo obavami. Kontaktujte nás a ozveme se vám co nejdříve.",
   },
 };
 
