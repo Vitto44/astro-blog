6686f1e80d5bad84f10a7a137bd4d28e579ec03b
Vito <vi.h.seo@gmail.com>
2024-11-02 16:25:14 +0100

    fix flags for language change, previed and seems good

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,6 +1,18 @@
+import enFlag from "@images/flagIcons/en.png";
+import skFlag from "@images/flagIcons/sk.png";
+import czFlag from "@images/flagIcons/cz.png";
+import ieFlag from "@images/flagIcons/ie.png";
+
 export const languages = {
   en: "England",
   sk: "Slovensko",
   cz: "Česko",
   ie: "Ireland",
 };
+
+export const flagImages = [
+  { lang: "en", src: enFlag },
+  { lang: "sk", src: skFlag },
+  { lang: "cz", src: czFlag },
+  { lang: "ie", src: ieFlag },
+];
